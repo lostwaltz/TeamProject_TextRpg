b01473e6e41d3aa0dc3297570916cce06a7d30d1
TheTimeOfRecord <131391796+TheTimeOfRecord@users.noreply.github.com>
2024-09-26 18:59:56 +0900

    Init Battle
    just init Battle

---
+++ Roronoa_TXT_RPG/Roronoa_TXT_RPG/Battle.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Roronoa_TXT_RPG
+{
+    internal class Battle
+    {
+        public void writeSceone()
+        {
+
+        }
+    }
+}
