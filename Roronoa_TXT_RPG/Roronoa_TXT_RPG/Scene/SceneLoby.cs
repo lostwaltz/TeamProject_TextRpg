b4a418c2be89b26e99bfa7db1865fb5909a4bf69
lostwaltz <lostwaltz89@gmail.com>
2024-09-26 18:59:54 +0900

    Update SceneLoby

--- Roronoa_TXT_RPG/Roronoa_TXT_RPG/Scene/SceneLoby.cs
+++ Roronoa_TXT_RPG/Roronoa_TXT_RPG/Scene/SceneLoby.cs
@@ -10,7 +10,24 @@ namespace Roronoa_TXT_RPG
     {
         public override void SceneUpdate()
         {
+            Console.Write(
+                "스파르타 던전에 오신 여러분 환영합니다.\n" +
+                "이제 전투를 시작할 수 있습니다.\n\n" +
+                "1. 상태 보기\n" +
+                "2. 전투 시작\n\n" +
+                "원하시는 행동을 입력해주세요.\n>> ");
+
             Program.KeyInputCheck(out int number, 10);
+
+            switch (number)
+            {
+                case 1:
+                    SceneManager.instance?.SceneChange(SCENE_TYPE.SCENE_STATUS);
+                    break;
+                case 2:
+                    SceneManager.instance?.SceneChange(SCENE_TYPE.SCENE_BATTLE);
+                    break;
+            }
         }
     }
 }
