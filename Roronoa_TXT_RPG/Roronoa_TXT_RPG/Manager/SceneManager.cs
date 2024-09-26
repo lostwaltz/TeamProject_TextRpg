bdd5cd1cac842cf7700956419e45510d57535c78
lostwaltz <lostwaltz89@gmail.com>
2024-09-26 17:36:56 +0900

    Add Scene System

---
+++ Roronoa_TXT_RPG/Roronoa_TXT_RPG/Manager/SceneManager.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+using Roronoa_TXT_RPG;
+
+namespace Roronoa_TXT_RPG
+{
+    class SceneManager
+    {
+        public static SceneManager? instance;
+
+        private Scene? _curScene;
+
+        public static void InitSceneManager()
+        {
+            if (instance == null)
+            {
+                instance = new SceneManager();
+            }
+        }
+
+        public void SceneChange(SCENE_TYPE sceneType)
+        {
+            switch (sceneType)
+            {
+                case SCENE_TYPE.SCENE_LOBY:
+                    _curScene = new SceneLoby();
+                    break;
+                case SCENE_TYPE.SCENE_STATUS:
+                    _curScene = new SceneStatus();
+                    break;
+                case SCENE_TYPE.SCENE_BATTLE:
+                    _curScene = new SceneBattle();
+                    break;
+            }
+        }
+    }
+}
---
+++ Roronoa_TXT_RPG/Roronoa_TXT_RPG/Scene/SceneStatus.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Roronoa_TXT_RPG
+{
+    internal class SceneStatus : Scene
+    {
+        public SceneStatus()
+        {
+        }
+
+        public override void SceneUpdate()
+        {
+        }
+    }
+}
