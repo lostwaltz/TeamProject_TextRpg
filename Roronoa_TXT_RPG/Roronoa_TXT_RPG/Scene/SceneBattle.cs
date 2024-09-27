dcf9313b697008635a24bdb93cab59a61826d6fc
TheTimeOfRecord <131391796+TheTimeOfRecord@users.noreply.github.com>
2024-09-27 13:46:50 +0900

    Remove all

--- Roronoa_TXT_RPG/Roronoa_TXT_RPG/Scene/SceneBattle.cs
+++ Roronoa_TXT_RPG/Roronoa_TXT_RPG/Scene/SceneBattle.cs
@@ -1,15 +1,27 @@
-﻿using System;
+﻿using Roronoa_TXT_RPG.Battle;
+using Roronoa_TXT_RPG.Monster;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection.Emit;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Linq;
+
+enum BATTLE_SCENE_TYPE { PLAYER_ACTION, PLAYER_ATTACK_ACTION, ENEMY_PHASE, BATTLE_RESULT}
 
 namespace Roronoa_TXT_RPG
 {
     internal class SceneBattle : Scene
     {
+
+
+
+
+        //Battle이 일하는 곳
         public override void SceneUpdate()
         {
+            Console.WriteLine("Battle");
         }
     }
 }
