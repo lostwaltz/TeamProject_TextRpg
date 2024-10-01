90318481f1d13973067033601e5b3f3d490a2240
parkseungwon11 <seungwp1020@naver.com>
2024-10-01 16:32:42 +0900

    Update Monster.cs

--- Roronoa_TXT_RPG/Roronoa_TXT_RPG/Monster/Monster.cs
+++ Roronoa_TXT_RPG/Roronoa_TXT_RPG/Monster/Monster.cs
@@ -4,9 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Roronoa_TXT_RPG.Monster
+namespace Roronoa_TXT_RPG
 {
-    internal class Monster
+    internal class Monster : Character
     {
         public string Name { get; set; }
         public int HealthPoint { get; set; }
@@ -14,13 +14,34 @@ namespace Roronoa_TXT_RPG.Monster
         public int Defense { get; set; }
         public int Speed { get; set; }
 
-        public Monster(string name, int healthpoint, int attackdemage, int defense, int speed)
+        public Monster(string name, int healthpoint, int attackdemage, int defense)
         {
             Name = name;
             HealthPoint = healthpoint;
             AttackDemage = attackdemage;
             Defense = defense;
-            Speed = speed;
         }
+
+        public override void PrintCharactorInfo() //몬스터 정보 출력 함수
+        {
+            Lv. { 레벨}
+            { 이름}
+            HP { 현재 체력} //MP {마나} (마나가 있다면)	
+        }
+
+        public void Attack()//기본공격
+        {
+            AttackOpponent(AttackPower);
+        }
+        /*
+        public void Skill()//스킬 이름별로 설정하면 될 듯
+        {
+            AttackOpponent(SkillPower);
+        }
+        */
+    }
+    internal class Goblin : Monster
+    {
+
     }
 }
