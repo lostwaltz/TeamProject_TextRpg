de4a7bd5c3d397e48b7bdb8978c9fdb1dcd41c8d
Hwijun-Yang <didgnlwns8728@naver.com>
2024-10-02 15:38:08 +0900

    바뀐점 모름

--- Roronoa_TXT_RPG/Roronoa_TXT_RPG/PlayerClass/CharacterClass.cs
+++ Roronoa_TXT_RPG/Roronoa_TXT_RPG/PlayerClass/CharacterClass.cs
@@ -3,28 +3,28 @@ namespace Roronoa_TXT_RPG
 {
     internal class Character
 	{
-        protected int level { get; set; }
-        protected string name { get; set; }
-        protected string job { get; set; }
-        protected int attackPower { get; set; }
-        protected int defense { get; set; }
-        protected int maxHealthPoint { get; set; }
-        protected int curHealthPoint { get; set; }
-        protected int maxManaPoint { get; set; }
-        protected int curManaPoint { get; set; }
-        protected int gold { get; set; }
-        protected bool isDead => curHealthPoint <= 0;
+        public int level { get; protected set; }
+        public string name { get; protected set; }
+        public string job { get; protected set; }
+        public int attackPower { get; protected set; }
+        public int defense { get; protected set; }
+        public int maxHealthPoint { get; protected set; }
+        public int curHealthPoint { get; protected set; }
+        public int maxManaPoint { get; protected set; }
+        public int curManaPoint { get; protected set; }
+        public int gold { get; protected set; }
+        public bool isDead => curHealthPoint <= 0;
 
         public int TakeDamage(int damage)
         {
             int _takeDamageHealthPoint = curHealthPoint - damage;
             if(_takeDamageHealthPoint < 0)
             {
-                Console.WriteLine($"{name}이(가){damage}만큼 데미지를 받아 사망했습니다. 현재체력: {takeDamageHealthPoint}");
+                Console.WriteLine($"{name}이(가){damage}만큼 데미지를 받아 사망했습니다. 현재체력: {_takeDamageHealthPoint}");
             }
             else
             {
-                Console.WriteLine($"{name}이(가){damage}만큼 데미지를 받았습니다. 현재체력: {takeDamageHealthPoint}");
+                Console.WriteLine($"{name}이(가){damage}만큼 데미지를 받았습니다. 현재체력: {_takeDamageHealthPoint}");
             }
             curHealthPoint = _takeDamageHealthPoint;
             return curHealthPoint;
---
+++ Roronoa_TXT_RPG/Roronoa_TXT_RPG/양휘준Class.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace Roronoa_TXT_RPG
+{
+	public class EmptyClass
+	{
+		public EmptyClass()
+		{
+		}
+	}
+}
+
