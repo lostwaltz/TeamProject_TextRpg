3c84bf3a92f34d3803bcb15fd1953e7ae2d9e166
lostwaltz <lostwaltz89@gmail.com>
2024-10-02 17:04:25 +0900

    fix character

--- Roronoa_TXT_RPG/Roronoa_TXT_RPG/PlayerClass/CharacterClass.cs
+++ Roronoa_TXT_RPG/Roronoa_TXT_RPG/PlayerClass/CharacterClass.cs
@@ -2,23 +2,38 @@
 namespace Roronoa_TXT_RPG
 {
     internal class Character
-	{
-        protected int level { get; set; }
-        protected string name { get; set; }
-        protected string job { get; set; }
-        protected int attackPower { get; set; }
-        protected int defense { get; set; }
-        protected int maxHealthPoint { get; set; }
-        public int curHealthPoint { get; private set; }
-        protected int maxManaPoint { get; set; }
-        protected int curManaPoint { get; set; }
-        protected int gold { get; set; }
+    {
+        public int level { get; protected set; }
+        public string name { get; protected set; }
+        public string job { get; protected set; }
+        public int attackPower { get; protected set; }
+        public int defense { get; protected set; }
+        public int maxHealthPoint { get; protected set; }
+        public int curHealthPoint { get; protected set; }
+        public int maxManaPoint { get; protected set; }
+        public int curManaPoint { get; protected set; }
+        public int gold { get; protected set; }
         public bool isDead => curHealthPoint <= 0;
+
+        public void DisplayInfo()
+        {
+            Console.WriteLine($"Lv.{level}");
+            Console.WriteLine($"{name} ({job})");
+            Console.WriteLine($"공격력: {attackPower}");
+            Console.WriteLine($"방어력: {defense}");
+            Console.WriteLine($"체력: {curHealthPoint}/{maxHealthPoint}");
+            Console.WriteLine($"Gold: {gold}G");
+            Console.WriteLine($" ");
+            Console.WriteLine($"0.나가기");
+            Console.WriteLine($" ");
+            Console.WriteLine($"원하시는 행동을 입력해주세요.");
+            Console.WriteLine($">>");
+        }
 
         public int TakeDamage(int damage)
         {
             int _takeDamageHealthPoint = curHealthPoint - damage;
-            if(_takeDamageHealthPoint < 0)
+            if (_takeDamageHealthPoint < 0)
             {
                 Console.WriteLine($"{name}이(가){damage}만큼 데미지를 받아 사망했습니다. 현재체력: {_takeDamageHealthPoint}");
             }
@@ -38,25 +53,25 @@ namespace Roronoa_TXT_RPG
             opponent.TakeDamage(damage);
         }
 
-        public virtual void PrintCharactorInfo()    
+        public virtual void PrintCharactorInfo()
         {
 
         }
 
         public virtual void PrintCharactorInfo(int befireBattlePlayerHealthPoint)
         {
-            if(curHealthPoint > 0)
+            if (curHealthPoint > 0)
             {
                 Console.WriteLine($"{level} {name}");
-                Console.WriteLine($"HP {beforeBattlePlayerHealthPoint}-> {curHealthPoint}");
+                Console.WriteLine($"HP {befireBattlePlayerHealthPoint}-> {curHealthPoint}");
             }
             else
             {
                 Console.WriteLine($"{level} {name}");
-                Console.WriteLine($"HP {beforeBattlePlayerHealthPoint}-> Dead");
+                Console.WriteLine($"HP {befireBattlePlayerHealthPoint}-> Dead");
             }
         }
-	}
-    
+    }
+
 }
 
